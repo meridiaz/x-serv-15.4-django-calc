9cb99dd0f5c3648c4b6b70da5ab81a6a430601ea
meri <mdiaz>
2020-03-20 19:47:56 +0100

    arreglado error en el manage.py

---
+++ cursosweb/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/meri/Escritorio/sat/gitlab/try2git/x-serv-15.4-django-calc/cursosweb/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
