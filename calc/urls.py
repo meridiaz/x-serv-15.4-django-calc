280885db947ebdfe3574d1f7d2f32e1fc0dfc6b6
meri <mdiaz>
2020-03-20 19:21:40 +0100

    arreglando error en un argumento

--- calc/urls.py
+++ calc/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('<str:op>/<str:op1>/<str:op2>', views.calcular),
+    path('<str:func>/<str:op1>/<str:op2>', views.calcular),
 ]
